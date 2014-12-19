934ddf0d9bce4ca77528213b6a963a0eb6161f1a
MilanistaDev <milanista224@gmail.com>
2014-12-19 22:04:29 +0900

    Initial Commit

---
+++ OCModalNSUserDefaultsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCModalNSUserDefaultsTest
+//
+//  Created by 麻生 拓弥 on 2014/12/19.
+//  Copyright (c) 2014年 Takuya Aso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCModalNSUserDefaultsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCModalNSUserDefaultsTest
+//
+//  Created by 麻生 拓弥 on 2014/12/19.
+//  Copyright (c) 2014年 Takuya Aso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
